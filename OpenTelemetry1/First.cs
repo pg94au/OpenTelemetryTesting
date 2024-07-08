7b0cc3a39521dffe13d69ba7f592dc319a91da05
BLACKBOX\paul <paul@grebenc.ca>
2024-07-08 17:09:34 -0400

    Starting with something...

---
+++ OpenTelemetry1/First.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
+
+namespace OpenTelemetry1;
+
+public class First
+{
+    public IDictionary<string, string> DoSomething()
+    {
+        var messageAttributes = new Dictionary<string, string>();
+
+        var activitySource = new ActivitySource("OpenTelemetry1");
+
+        var activity = activitySource.StartActivity(ActivityKind.Producer);
+
+        if (activity != null)
+        {
+            Propagators.DefaultTextMapPropagator.Inject(
+                new PropagationContext(activity.Context, Baggage.Current),
+                messageAttributes,
+                (attributes, key, value) => messageAttributes[key] = value
+            );
+        }
+
+        return messageAttributes;
+    }
+}
---
+++ OpenTelemetry1/Second.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTelemetry.Context.Propagation;
+
+namespace OpenTelemetry1;
+
+public class Second
+{
+    public void DoSomething(IDictionary<string, string> messageAttributes)
+    {
+        var context = Propagators.DefaultTextMapPropagator.Extract(
+            default,
+            messageAttributes,
+            (attributes, key) => attributes.TryGetValue(key, out var value) ? new[] { value } : Array.Empty<string>());
+
+        var activitySource = new ActivitySource("OpenTelemetry1");
+
+        using var activity = activitySource.StartActivity(
+            "Second",
+            ActivityKind.Consumer,
+            default(string?),
+            new List<KeyValuePair<string, object?>>(),
+            new[] { new ActivityLink(context.ActivityContext) });
+    }
+}
